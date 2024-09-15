91f1aa701d8e64b1d16be29b4bd272fafbe472d8
Coder Poet <2332778528@qq.com>
2024-09-15 14:15:26 +0800

    feat: vue router init

---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import type { RouteRecordRaw } from "vue-router";
+
+type RouteRecord = RouteRecordRaw & { title?: string };
+
+export const routes: readonly RouteRecord[] = [
+  {
+    path: "",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
+  },
+];
