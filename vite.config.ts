93faa9282cf124124c3d9d624f496e3dbfe45e43
Coder Poet <2332778528@qq.com>
2024-09-15 14:33:33 +0800

    feat: use unplugin auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
 import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      imports: ["vue", "vue-router", "pinia"],
+      dts: true,
+    }),
     vue(),
     VueDevTools({
       launchEditor: "code-insiders", // Your editor
