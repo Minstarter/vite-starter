4c69d6870a2f7106d15d7d524091b2249c78b041
Coder Poet <2332778528@qq.com>
2024-09-15 14:15:59 +0800

    feat: use vue dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VueDevTools({
+      launchEditor: "code-insiders", // Your editor
+    }),
+  ],
+});
