ef3e901ae7d568fa6f91e07b292160002b2d51d8
Coder Poet <2332778528@qq.com>
2024-09-15 14:47:49 +0800

    feat: use dayjs

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
@@ -6,7 +7,10 @@ import router from "./router";
 
 import "./style.css";
 
+import "dayjs/locale/zh-cn";
 import "element-plus/es/hooks/use-locale/index";
+
+dayjs.locale("zh-cn");
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
