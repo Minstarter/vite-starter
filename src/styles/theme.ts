4ffd6ae88de9399e7a40b9115de997966c286011
Coder Poet <2332778528@qq.com>
2024-09-15 17:01:17 +0800

    feat: use unocss theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const light = {
+  colors: {
+    text: "#7cb9ff",
+  },
+};
+
+export const dark = {
+  colors: {
+    text: "#ff7c7c",
+  },
+};
--- uno.config.ts
+++ uno.config.ts
@@ -5,9 +5,13 @@ import {
   presetUno,
   transformerDirectives,
 } from "unocss";
+import type { Theme } from "unocss/preset-uno";
 
 import presetIcons from "@unocss/preset-icons";
 import presetRemToPx from "@unocss/preset-rem-to-px";
+import presetTheme from "unocss-preset-theme";
+
+import { dark, light } from "./src/styles/theme";
 
 export default defineConfig({
   shortcuts: {
@@ -25,6 +29,7 @@ export default defineConfig({
       /* preset icons options */
     }),
     presetRemToPx({ baseFontSize: 4 }),
+    presetTheme<Theme>({ theme: { dark, light } }),
     presetUno(),
   ],
   transformers: [transformerDirectives()],
